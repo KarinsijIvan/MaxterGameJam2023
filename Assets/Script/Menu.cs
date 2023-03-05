e4b4e5124207ac13155c8bbb916dce11a0d6749b
Alex <Tolstov777Alex@yandex.ru>
2023-03-05 21:51:06 +0700

    FirstCommit

---
+++ Assets/Script/Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    [SerializeField] GameObject stMenuPanel, authorPanel;
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void OpenPanelAuthor()
+    {
+        stMenuPanel.SetActive(false);
+        authorPanel.SetActive(true);
+    }
+
+    public void ClosePanelAuthor()
+    {
+        stMenuPanel.SetActive(true);
+        authorPanel.SetActive(false);
+    }
+}
